2bd106b49fac6aadcac6a0fe7b6d95c3aa88811c
Nathan Smith <natsmith@digitalriver.com>
2016-03-08 13:40:58 -0600

    fixing some styling

--- example/components/Buttons/Buttongroups.js
+++ example/components/Buttons/Buttongroups.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react'
 import { ButtonGroup, Button } from 'react-bootstrap'
 
 export default class ButtonGroups extends Component {
+static displayName = "ButtonGroups";
   static styleguide = {
     index: '1.2',
     category: 'Buttons',
     title: 'Button groups',
     description: 'Group a series of buttons together on a single line with the button group.',
+    example: `
+<ButtonGroup>
+    <Button>Left</Button>
+    <Button>Middle</Button>
+    <Button>Right</Button>
+</ButtonGroup>`
   }
 
   render () {
