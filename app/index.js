8d791ff55e2e5a1cca2f0754012e1762a1add38a
Nathan Smith <natsmith@digitalriver.com>
2016-03-22 15:05:00 -0500

    make root option work with react-router 2.0.0

--- app/index.js
+++ app/index.js
@@ -1,8 +1,16 @@
 /*global document:false*/
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, browserHistory} from 'react-router'
+import { createHistory } from 'history'
+import { Router, useRouterHistory } from 'react-router'
 import routes from './routes'
+import path from '../rcs-tmp/root-path'
 import './index.css'
+
+var root = path ? path : '/';
+
+const browserHistory = useRouterHistory(createHistory)({
+    basename: root
+});
 
 render(<Router routes={routes} history={ browserHistory } />, document.getElementById('app'));
